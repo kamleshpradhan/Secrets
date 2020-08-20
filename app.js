e739c4190f2b66672d66f7a20436e06ef7bb2445
unknown <>
2020-08-20 15:43:40 +0530

    Add environment file

---
+++ app.js
@@ -0,0 +1,116 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require("express")
+const bodyParser = require("body-parser")
+const ejs = require("ejs")
+const mongoose = require("mongoose")
+const encrypt = require("mongoose-encryption");
+
+
+
+console.log(process.env.API_KEY);
+
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set("view engine","ejs")
+
+
+mongoose.connect('mongodb://localhost:27017/userdb', {useNewUrlParser: true,useUnifiedTopology: true });
+
+const usersSchema = new mongoose.Schema({
+    email: String,
+    password: String
+})
+
+usersSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields:["password"]});
+
+
+
+const users = new mongoose.model('User',usersSchema);
+
+app.get('/',function(req,res){
+    res.render("home")
+});
+app.get("/Login",function(req,res){
+    res.render("login")
+})
+app.get('/register',function(req,res){
+    res.render("register")
+})
+// app.get('/secrets',function(req,res){
+//     res.render("secrets")
+// })
+// app.get('/submit',function(req,res){
+//     res.render("submit")
+// })
+
+app.post('/register',function(req,res){
+    const newUser = new users({
+        email:req.body.username,
+        password:req.body.password
+    })
+    newUser.save(function(err){
+        if (err){
+            console.log(err)
+        }else{
+            res.render('secrets')
+        }
+    })
+})
+
+app.post('/login',function(req,res){
+    const username = req.body.username
+    const password = req.body.password
+    users.findOne({email:username},function(err,founduser){
+        if(err){
+            console.log(err)
+        }else{
+            if(founduser){
+                if(founduser.password===password){
+                    res.render('secrets')
+                }
+            }
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(req,res){
+    console.log("Server started on port 3000")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
